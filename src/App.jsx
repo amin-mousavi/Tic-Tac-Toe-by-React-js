2d46a70a92b56dd8a5ea5484c65b8447ccf85b8d
Amin Mousavi <seyedamin963@gmail.com>
2023-11-27 22:14:26 +0330

    create tic-tac-toe game board and show winner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,87 @@
+import { useState } from "react";
+
+import GameBoard from "./components/GameBoard";
 import Player from "./components/player";
+import Log from "./components/Log";
+
+import { WINNING_COMBINATIONS } from "./winning-combination";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const {
+      square: { row, col },
+      player,
+    } = turn;
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    // console.log('sik');
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameTurns((prevTurn) => {
+      const currentPlayer = derivedActivePlayer(prevTurn);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurn,
+      ];
+      return updatedTurns;
+    });
+  };
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        Game BOARD
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      LOG
+      <Log turns={gameTurns} />
     </main>
   );
 }
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const GameBoard = ({ onSelectSquare, board }) => {
+
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playersSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playersSymbol !== null}
+                >
+                  {playersSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
